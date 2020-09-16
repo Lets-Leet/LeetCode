d7025258a029a18c876cdd0e3ecff4f38f3ae0f7
shraddhak22 <spkharche22@gmail.com>
2020-09-15 21:32:46 -0400

    310: Minimum height tress

---
+++ 310/solution.js
@@ -0,0 +1,39 @@
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @return {number[]}
+ */
+var findMinHeightTrees = function (n, edges) {
+    if (!edges || n < 2) return [0];
+    let graph = [];
+    // parse edges
+    for (let [x, y] of edges) {
+
+        graph[x] = graph[x] || [];
+        graph[y] = graph[y] || [];
+        graph[x].push(y);
+        graph[y].push(x);
+
+    }
+    let leaves = [];
+    // init leaf nodes
+    graph.map((pts, i) => { pts.length === 1 && leaves.push(i) });
+    while (n > 2) {
+        n = n - leaves.length;
+        let nxt_leaves = [];
+        for (let leave of leaves) {
+            // remove leaf node and itself in related nodes
+            tmp = graph[leave].pop();
+            console.log(tmp)
+            console.log(graph)
+            graph[tmp].splice(graph[tmp].indexOf(leave), 1);
+            console.log(graph)
+            console.log("-------------------------------")
+            // save new leaf node
+            graph[tmp].length === 1 && nxt_leaves.push(tmp);
+            //console.log("Next leaves---> " + nxt_leaves)
+        }
+        leaves = nxt_leaves;
+    }
+    return leaves;
+};
